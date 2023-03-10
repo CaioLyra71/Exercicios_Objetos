9951acd8abaf366e12ac06d7490b4a3447240aad
CaioLyra71 <caiolyra1@hotmail.com>
2023-03-10 15:50:24 -0300

    Exercicio 01 finalizado

---
+++ Exercicio 01.js
@@ -0,0 +1,21 @@
+const endereco = {
+    rua: "Rua do zero ao DEV",
+    numero: 10,
+    complemento: "Apto 100",
+    bairro: "java",
+    cep: "10000-000",
+    cidade: "Salvador",
+    estado: "Bahia",
+    pais: "Brasil"
+}
+
+const usuario = {
+    nome: "Caio",
+    email: "abcde@hotmail.com",
+    telefone: "(71)99999-9999",
+    data_nascimento: "08/05/1992",
+    endereco
+
+}
+
+console.log(usuario)
