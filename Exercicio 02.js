81e4301d9ff90008be32ef2dbd368ceb898d8b8a
CaioLyra71 <caiolyra1@hotmail.com>
2023-03-10 15:57:00 -0300

    Exercicio 02 finalizado

---
+++ Exercicio 02.js
@@ -0,0 +1,31 @@
+const carros = [
+    {
+        marca: "Volkswagen",
+        modelo: "Gol",
+        ano: 2022,
+        cor: "vermelho",
+        quantidade_portas: 4,
+        automatico: false
+    },
+
+    {
+        marca: "Toyota",
+        modelo: "Yaris",
+        ano: 2021,
+        cor: "branco",
+        quantidade_portas: 4,
+        automatico: true
+    },
+
+    {
+        marca: "Hyundai",
+        modelo: "HB20",
+        ano: 2019,
+        cor: "prata",
+        quantidade_portas: 4,
+        automatico: false
+    }
+
+]
+
+console.log(carros)
