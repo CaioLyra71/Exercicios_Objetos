14135124b3b1713ee7606786bcaee79150343e4d
CaioLyra71 <caiolyra1@hotmail.com>
2023-03-11 15:34:54 -0300

    Exercicio 09 finalizado

---
+++ Exercício 09.js
@@ -0,0 +1,28 @@
+const participantes = [
+    { nome: "João" },
+    { nome: "Ana" },
+    { nome: "Beatriz" },
+    { nome: "Maria" },
+    { nome: "Ana Clara" },
+    { nome: "Joana" },
+    { nome: "Augusto" },
+    { nome: "Renan" },
+    { nome: "Patricia" },
+    { nome: "Carlos" },
+    { nome: "Renato" },
+    { nome: "José" },
+    { nome: "Roberto" },
+    { nome: "Sara" },
+    { nome: "Junior" },
+    { nome: "Pedro" },
+    { nome: "Vitor" },
+    { nome: "Antonio" },
+]
+let i = 0
+for (participante of participantes) {
+    if (participante.nome === "Carlos") {
+        console.log(`Galera... O Carlos está na posição ${i}, corre lá!`)
+    }
+    i++
+}
+
